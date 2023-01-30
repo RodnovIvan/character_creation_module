a544e3ccd77a9fbbb1a174b5fd12f7f48c47fe05
Иван Роднов <rodnovivan3@gmail.com>
2023-01-30 03:43:01 +0300

    Добавить аннотации, документацию, все в pep8

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+"""Основной модуль игры.
+Выводит текст для игрока, позволяет игроку выбрать
+класс, провести тренировку: блокировать, атаковать и
+использовать специальные умения с помощью соответствующих
+функций.
+"""
+
 from random import randint
+
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Принять на вход игровой класс и имя игрока.
+    В зависимости от класса провести рассчёт атаки
+    и вернуть значение с игровым комментарием.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f'{5 + randint(3, 5)}')
@@ -15,6 +28,10 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Принять на вход игровой класс и имя игрока.
+    В зависимости от класса провести рассчёт блока
+    и вернуть значение с игровым комментарием.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +42,10 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Принять на вход игровой класс и имя игрока.
+    В зависимости от класса рассчитать спец. умение
+    и вернуть значение с игровым комментарием.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f'«Выносливость {80 + 25}»')
@@ -36,6 +57,12 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Принять на вход игровой класс и имя игрока.
+    Вывести игр. класс игрока с описанием, предложить
+    тренировку и команды для нее. Сохранить ввод
+    игрока и запустить другие функции в зависимости
+    от ввода(атака, защита, спец. умение.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +87,9 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Инициировать выбор игр.класса.
+    сохранить и вернуть выбранный игр. класс.
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -81,7 +111,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -91,6 +122,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
